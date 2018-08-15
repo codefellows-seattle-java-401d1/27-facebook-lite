c852906e178937addad6932dc2bdbb638aea64fc
Paul Suarez <paulsblogging.com@gmail.com>
2018-08-15 13:29:30 -0700

    finally got it working

---
+++ app/src/main/java/com/example/paulsuarez/a27_facebook_lite/Friend.java
@@ -0,0 +1,12 @@
+package com.example.paulsuarez.a27_facebook_lite;
+
+public class Friend {
+    public String name;
+    public String bio;
+
+    public Friend(String name, String bio) {
+        this.name = name;
+        this.bio = bio;
+    }
+
+}
