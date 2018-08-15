8724318df2e7667f9cec9752c68ea0c1bf9a4789
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-08-14 20:44:47 -0700

    Intro page, and friend list.

---
+++ lab-nick/app/src/main/java/com/example/facebooklite/FriendsActivity.java
@@ -0,0 +1,44 @@
+package com.example.facebooklite;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.facebooklite.models.Friend;
+
+public class FriendsActivity extends AppCompatActivity{
+
+    private Button mTyler;
+    private Button mPaul;
+    private Button mLuther;
+    private Button mBrandiboi;
+    private Button mTara;
+
+    //Creating an array of Friends named friends.
+    Friend[] friends = {
+            new Friend("Tyler", "I'm a home schooled vapey boi who loves long walks on Skellige Isle with Geralt of Rivia. 32 Vape Lyfe."),
+            new Friend("Paul", "My hobbies include vaping, eating the food of my people, and stealing handles of other peoples liquor."),
+            new Friend("Luther", "My hobbies include the Python Lyfe, and making these guns look amazing. You see this hair? Stylin' and profilin'!"),
+            new Friend("Brandiboi", "I like swiping right and climbing mountains with the bois. Ramen is life, and you can't keep a good corn dog down. #EverestOneDay"),
+            new Friend("Tara", "I'm a skilled programmer with a lot of self confidence issues who doesn't believe what the bois say when they tell me I am the best programmer."),
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friends_activity);
+
+        Intent intent = getIntent();
+
+        mTyler = findViewById(R.id.tyler);
+        mPaul = findViewById(R.id.paul);
+        mLuther = findViewById(R.id.luther);
+        mBrandiboi = findViewById(R.id.brandiboi);
+        mTara = findViewById(R.id.tara);
+
+    }
+
+
+}
--- lab-nick/app/src/main/java/com/example/facebooklite/MainActivity.java
+++ lab-nick/app/src/main/java/com/example/facebooklite/MainActivity.java
@@ -1,17 +1,36 @@
 package com.example.facebooklite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.facebooklite.models.Friend;
 
 public class MainActivity extends AppCompatActivity {
-    
-
+    public static final int REQUEST_CHOOSE_FRIEND = 1;
+    private TextView intro;
+    private Button seeFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Creating our text views
+        intro = findViewById(R.id.intro);
 
+        // Creating our buttons
+        seeFriends = findViewById(R.id.seeFriends);
+
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, FriendsActivity.class);
+            startActivityForResult(intent, REQUEST_CHOOSE_FRIEND);
+        }
     }
+
+
 }
