2bf15310d642fe3843c913c9f9b7e7fa545f223c
brandon-fenty <brandonfenty@gmail.com>
2018-08-16 12:28:28 -0700

    button on main page wired up to 2nd activity

--- app/src/main/java/com/yutani/codefellows/a27_facebook_lite/MainActivity.java
+++ app/src/main/java/com/yutani/codefellows/a27_facebook_lite/MainActivity.java
@@ -1,7 +1,9 @@
 package com.yutani.codefellows.a27_facebook_lite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -15,6 +17,12 @@ public class MainActivity extends AppCompatActivity {
 
         mSeeFriendsButton = findViewById(R.id.friend_button);
 
-
+        mSeeFriendsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FriendsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
