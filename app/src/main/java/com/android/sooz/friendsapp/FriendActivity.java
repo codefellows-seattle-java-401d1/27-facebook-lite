17536a8f457df217b27d884d95f7609419f94a7d
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-16 13:28:09 -0700

    Thanks to help from Casey discovered problem that was crashing app - can't have string resources in friend array

--- app/src/main/java/com/android/sooz/friendsapp/FriendActivity.java
+++ app/src/main/java/com/android/sooz/friendsapp/FriendActivity.java
@@ -14,11 +14,11 @@ import com.android.sooz.friendsapp.models.Friend;
 public class FriendActivity extends AppCompatActivity {
 
     Friend[] friends = {
-            new Friend("Little Mac & Big Tony", R.drawable.littlemac_bigtony, getString(R.string.bio_mactony), "@littlemac.bigtony"),
-            new Friend( "Tortilla & Chulula", R.drawable.gatos_hermans, getString(R.string.bio_gatos), "@gatos_hermanas"),
-            new Friend("Stewie & Simon", R.drawable.stewie_simon, getString(R.string.bio_stewiesimon), "@stewie_and_simon"),
-            new Friend("Maple Cat", R.drawable.maplecat, getString(R.string.bio_maple), "@maple.cat"),
-            new Friend("Stevie Nicks", R.drawable.stevie_devonrex, getString(R.string.bio_stevie), "@steviethedevonrex")
+            new Friend("Little Mac Big Tony", R.drawable.littlemac_bigtony, "Little Mac is not a video game boxer and Big Tony is not a mobster. Just two boys from the District living with a guy, all thanks to @kittenxlady.", "@littlemac.bigtony"),
+            new Friend("Tortilla Chulula", R.drawable.gatos_hermans, "Sister cats // Seattle, WA", "@gatos_hermanas"),
+            new Friend("Stewie Simon", R.drawable.stewie_simon, "Stewie - 5 y/o cameo tabby American Shorthair, born with obstructive hypertrophic cardiomyopathy \uD83D\uDC31 Simon - 5 y/o brown tabby American Shorthair", "@stewie_and_simon"),
+            new Friend("Maple Cat", R.drawable.maplecat, "Little Maplekins \uD83C\uDF41 \uD83C\uDF41 Mr. Plumpy Bear ✨ Golden British shorthair \uD83C\uDFE1 Los Angeles \uD83C\uDF82 6.21.17 \uD83D\uDC31 Sisters @plumpycats \uD83D\uDCDA We post lots of cute stories \uD83D\uDE4F\uD83C\uDFFB Please credit all reposts facebook.com/themaplecat", "@maple.cat"),
+            new Friend("Stevie Nicks", R.drawable.stevie_devonrex, "Stevie Nicks The Devon Rex \uD83D\uDCCDSeattle \uD83C\uDF1C 4.6.18 \uD83D\uDC08 Devon Rex ♀️ \uD83E\uDD26\uD83C\uDFFC\u200D♀️ @sharellkatelynn", "@steviethedevonrex")
     };
 
     //variables for content on each app page/view
@@ -36,19 +36,21 @@ public class FriendActivity extends AppCompatActivity {
     private Button backToMain;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
 
-//        Intent intent = getIntent();
-////
-//        button = findViewById(R.id.friendButton1);
-//
-//        showFriends();
-
+        backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FriendActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+}
 
 //
 //    //creates list of fir
@@ -65,4 +67,3 @@ public class FriendActivity extends AppCompatActivity {
 //    }
 
 
-}
