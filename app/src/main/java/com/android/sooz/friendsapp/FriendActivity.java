00d59640ed99fd38b5363038b962fdadb56faf76
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-17 08:40:41 -0700

    took out drawable ID due to it generating a compiling error I don't yet know how to fix, reset friend activity page to get one button working and it works! Need to hook up remaining buttons and finish up Friend Detail Activty.

--- app/src/main/java/com/android/sooz/friendsapp/FriendActivity.java
+++ app/src/main/java/com/android/sooz/friendsapp/FriendActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,24 +15,23 @@ import com.android.sooz.friendsapp.models.Friend;
 public class FriendActivity extends AppCompatActivity {
 
     Friend[] friends = {
-            new Friend("Little Mac Big Tony", R.drawable.littlemac_bigtony, "Little Mac is not a video game boxer and Big Tony is not a mobster. Just two boys from the District living with a guy, all thanks to @kittenxlady.", "@littlemac.bigtony"),
-            new Friend("Tortilla Chulula", R.drawable.gatos_hermans, "Sister cats // Seattle, WA", "@gatos_hermanas"),
-            new Friend("Stewie Simon", R.drawable.stewie_simon, "Stewie - 5 y/o cameo tabby American Shorthair, born with obstructive hypertrophic cardiomyopathy \uD83D\uDC31 Simon - 5 y/o brown tabby American Shorthair", "@stewie_and_simon"),
-            new Friend("Maple Cat", R.drawable.maplecat, "Little Maplekins \uD83C\uDF41 \uD83C\uDF41 Mr. Plumpy Bear ✨ Golden British shorthair \uD83C\uDFE1 Los Angeles \uD83C\uDF82 6.21.17 \uD83D\uDC31 Sisters @plumpycats \uD83D\uDCDA We post lots of cute stories \uD83D\uDE4F\uD83C\uDFFB Please credit all reposts facebook.com/themaplecat", "@maple.cat"),
-            new Friend("Stevie Nicks", R.drawable.stevie_devonrex, "Stevie Nicks The Devon Rex \uD83D\uDCCDSeattle \uD83C\uDF1C 4.6.18 \uD83D\uDC08 Devon Rex ♀️ \uD83E\uDD26\uD83C\uDFFC\u200D♀️ @sharellkatelynn", "@steviethedevonrex")
+            new Friend("Little Mac Big Tony", "Little Mac is not a video game boxer and Big Tony is not a mobster. Just two boys from the District living with a guy, all thanks to @kittenxlady.", " Find us at @littlemac.bigtony"),
+            new Friend("Tortilla Chulula", "Sister cats // Seattle, WA", " Find us @gatos_hermanas"),
+            new Friend("Stewie Simon","Stewie - 5 y/o cameo tabby American Shorthair, born with obstructive hypertrophic cardiomyopathy \uD83D\uDC31 Simon - 5 y/o brown tabby American Shorthair", "Find us @stewie_and_simon"),
+            new Friend("Maple Cat",  "Little Maplekins \uD83C\uDF41 \uD83C\uDF41 Mr. Plumpy Bear ✨ Golden British shorthair \uD83C\uDFE1 Los Angeles \uD83C\uDF82 6.21.17 \uD83D\uDC31 Sisters @plumpycats \uD83D\uDCDA We post lots of cute stories \uD83D\uDE4F\uD83C\uDFFB Please credit all reposts facebook.com/themaplecat", "Find me @maple.cat"),
+            new Friend("Stevie Nicks", "Stevie Nicks The Devon Rex \uD83D\uDCCDSeattle \uD83C\uDF1C 4.6.18 \uD83D\uDC08 Devon Rex ♀️ \uD83E\uDD26\uD83C\uDFFC\u200D♀️ @sharellkatelynn", "Find me @steviethedevonrex")
     };
 
     //variables for content on each app page/view
-    private ImageView picture;
+
     private TextView name;
     private TextView bio;
     private TextView instaHandle;
 
-    private Button mfriendButton1;
-    private Button mfriendButton2;
-    private Button mfriendButton3;
-    private Button mfriendButton4;
-    private Button mfriendButton5;
+    //seems to be breaking application so shelving concept for now
+    private ImageView picture;
+
+    private Button mfriendChoice;
 
     private Button backToMain;
 
@@ -50,16 +50,23 @@ public class FriendActivity extends AppCompatActivity {
             }
         });
 
-        mfriendButton1 = findViewById(R.id.friendButton1);
-        mfriendButton1.setOnClickListener(new View.OnClickListener() {
+        //friend 1
+
+        name = findViewById(R.id.name);
+        Friend friend1 = friends[0];
+
+        Log.d("friend1","Hi my name is "+friend1.name+".");
+
+        mfriendChoice = findViewById(R.id.friendButton1);
+
+        mfriendChoice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(FriendActivity.this, FriendDetailActivity.class);
 
-                intent.putExtra(friends[0].drawableID);
-                intent.putExtra(friends[0].instaHandle);
-                intent.putExtra (friends[0].name);
-                intent.putExtra(friends[0].bio);
+                intent.putExtra("name", friends[0].name);
+                intent.putExtra("bio", friends[0].bio);
+                intent.putExtra("instaHandle", friends[0].instaHandle);
 
                 startActivity(intent);
             }
--- app/src/main/java/com/android/sooz/friendsapp/FriendDetailActivity.java
+++ app/src/main/java/com/android/sooz/friendsapp/FriendDetailActivity.java
@@ -1,17 +1,42 @@
 package com.android.sooz.friendsapp;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.android.sooz.friendsapp.models.Friend;
 
 public class FriendDetailActivity extends AppCompatActivity {
 
+    private Button backToMain;
+    private Button backFriendsList;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend_detail);
+
+        backFriendsList = findViewById(R.id.backFriendList);
+        backFriendsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FriendDetailActivity.this, FriendActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backToMain = findViewById(R.id.backToMain);
+        backToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FriendDetailActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
